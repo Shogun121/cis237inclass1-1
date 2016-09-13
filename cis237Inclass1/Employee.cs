cbda50fa047e33ddc458935b08aeb20fb92a4b4d
Robert Cooely <Daimyo121@aol.com>
2016-09-13 16:52:25 -0400

    Add Employee class and some code to main

---
+++ cis237Inclass1/Employee.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237Inclass1
+{
+    class Employee
+    {
+        //****************************************
+        //Backing Fields
+        //****************************************
+        private string _firstName;
+        private string _lastName;
+        private decimal _weeklySalary;
+
+        //*****************************************
+        //Properties
+        //*****************************************
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+        public decimal WeeklySalary
+        {
+            get { return _weeklySalary; }
+            set { _weeklySalary = value; }
+        }
+
+        //*****************************
+        //Public methods
+        //*****************************
+        public override string ToString()
+        {
+            return this._firstName + " " + this._lastName;
+        }
+        public decimal YearlySalary()
+        {
+            return this._weeklySalary * 52;
+        }
+        
+        //********************************
+        //Constructor
+        //********************************
+        public Employee(string firstName, string lastName, decimal weeklySalary)
+        {
+            this._firstName = firstName;    //backing field set to parameters
+            this._lastName = lastName;
+            this._weeklySalary = weeklySalary;
+        }
+        public Employee()
+        {
+            //Do nothing
+        }
+
+    }
+    
+}
--- cis237Inclass1/Program.cs
+++ cis237Inclass1/Program.cs
@@ -10,6 +10,30 @@ namespace cis237Inclass1
     {
         static void Main(string[] args)
         {
+            Employee myEmployee = new Employee();
+            myEmployee.FirstName = "Robert";
+            myEmployee.LastName = "Cooley";
+            myEmployee.WeeklySalary = 2048.54m;
+
+            Console.WriteLine(myEmployee.FirstName);
+            Console.WriteLine(myEmployee);
+
+            Employee[] employees = new Employee[10];    //Create an array
+
+            employees[0] = new Employee("James", "Kirk", 453.00m);
+            employees[1] = new Employee("Jean-Luc", "Picard", 200m);
+            employees[2] = new Employee("Benjamin", "Sisko", 190.00m);
+            employees[3] = new Employee("Kathryn", "Janeway", 897.00m);
+            employees[4] = new Employee("Johnathan", "Archer", 425.00m);
+
+            foreach (Employee employee in employees)
+            {
+                if(employee != null)
+                {
+                    Console.WriteLine(employee.ToString() + " " + employee.YearlySalary());
+                }
+                
+            }
         }
     }
 }
